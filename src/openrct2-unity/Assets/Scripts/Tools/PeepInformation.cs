2f962e3e47279b66991c66a023dac34cb4ac690f
Rolf <info@rolfoldenkotte.nl>
2020-04-20 21:39:48 +0200

    added Peep label names when hovering

--- src/openrct2-unity/Assets/Scripts/Tools/PeepInformation.cs
+++ src/openrct2-unity/Assets/Scripts/Tools/PeepInformation.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PeepInformation : MonoBehaviour
 {
     private string name;
-    void OnMouseDown()
+    public Text nameLabel;
+    void OnMouseOver()
     {
-        Debug.Log("Clicked: " + name);
+        nameLabel.GetComponent<Text>().enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        nameLabel.GetComponent<Text>().enabled = false;
+    }
+
+    void Update()
+    {
+        Vector3 namePose = Camera.main.WorldToScreenPoint(gameObject.transform.Find("Sphere").transform.position);
+        nameLabel.transform.position = namePose;
     }
 
     public void UpdateInformation(OpenRCT2.Unity.Peep peep)
     {
         name = $"{peep.type} {peep.Id}";
+        gameObject.transform.Find("Canvas").Find("Text").GetComponent<UnityEngine.UI.Text>().text = name;
         /*
         var intensityBinary = System.Convert.ToString(peep.intensity, 2);
         var maxIntensityString = System.Convert.ToString(peep.intensity >> 4);
