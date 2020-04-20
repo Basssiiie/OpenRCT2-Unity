8eaeffcd3d86d67d5dfd6a45c9447546b56ff112
Rolf <info@rolfoldenkotte.nl>
2020-04-20 19:53:52 +0200

    added more to Peep, PeepInformation simplified, and more enums

--- src/openrct2-unity/Assets/Scripts/Tools/PeepInformation.cs
+++ src/openrct2-unity/Assets/Scripts/Tools/PeepInformation.cs
@@ -4,61 +4,22 @@ using UnityEngine;
 
 public class PeepInformation : MonoBehaviour
 {
-    /* 
-     public int value; // will show
-     [SerializeField] private int serPrivValue; // will show
-     [SerializeField] private int serProtValue; // will show      
-     private int privValue; // will not show
-     protected int protValue; // will not show
-    [Header("Button Settings")]
-        [Tooltip("Arbitary text message")]
-  */
-
-    [Header("Peep General Information")]
-    public string name;
-    public string type;
-    public string insideThePark;
-    public string timeInPark;
-
-    [Header("Peep Activity")]
-    public string state;
-    public string subState;
-    public string ridesDone;
-    public string TryingToGetTo;
-    public string DestinationTolerance;
-    public string intensity;
-    public string nauseaTolerance;
-    public string actionSprite;
-
-    [Header("Peep Needs")]
-    [Range(0, 256)] public int energy;
-    [Range(0, 256)] public int happiness;
-    [Range(0, 256)] public int hunger;
-    [Range(0, 256)] public int thirst;
-    [Range(0, 256)] public int toilet;
-
+    private string name;
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked: " + name);
+    }
 
     public void UpdateInformation(OpenRCT2.Unity.Peep peep)
     {
-        name = $"{(peep.Name != null ? peep.Name : "null")}";
-        type = $"{peep.type}";
-        state = $"{peep.state}";
-        insideThePark = $"{(peep.outsideOfPark == 0 ? "Yes" : "No")}";
-        subState = $"{peep.substate}";
-        ridesDone = $"{peep.staffTypeOrNoOfRides}";
-        TryingToGetTo = $"{(peep.destinationX + peep.destinationY)}";
-        DestinationTolerance = $"{peep.destinationTolerance}";
+        name = $"{peep.type} {peep.Id}";
+        /*
+        var intensityBinary = System.Convert.ToString(peep.intensity, 2);
+        var maxIntensityString = System.Convert.ToString(peep.intensity >> 4);
+        var minIntensityString = System.Convert.ToString(peep.intensity & 0b1111);
 
-        energy = peep.energy;
-        happiness = peep.happiness;
-        hunger = peep.hunger;
-        thirst = peep.thirst;
-        toilet = peep.toilet;
-
-        intensity = $"{peep.intensity}";
-        nauseaTolerance = $"{peep.nauseaTolerance}";
-        actionSprite = $"{peep.actionSpriteType}";
-        timeInPark = $"{peep.timeInPark}";
-
+        minIntensity = $"{minIntensityString}";
+        maxIntensity = $"{maxIntensityString}";
+        */
     }
 }
