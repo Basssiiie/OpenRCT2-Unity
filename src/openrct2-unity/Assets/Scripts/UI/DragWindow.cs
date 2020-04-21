383d4a6acc9d2f259d0be827bb16c7a184415094
Rolf <info@rolfoldenkotte.nl>
2020-04-21 21:39:28 +0200

    added peep window

---
+++ src/openrct2-unity/Assets/Scripts/UI/DragWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private GameObject peepWindow;
+    [SerializeField] private Canvas canvas;
+    public void Awake()
+    {
+        if (dragRectTransform == null)
+        {
+            dragRectTransform = dragRectTransform.parent.GetComponent<RectTransform>();
+        }
+
+        if (canvas == null)
+        {
+            canvas = FindObjectOfType<Canvas>().transform.GetComponent<Canvas>();
+        }
+    }
+
+
+    public void CloseWindow()
+    {
+        Destroy(peepWindow);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+}
