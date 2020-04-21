164d43ce498b65fdb4c422213eccd0cadd84d664
Rolf <info@rolfoldenkotte.nl>
2020-04-21 22:16:57 +0200

    hotfix

--- src/openrct2-unity/Assets/Scripts/UI/PeepInformation.cs
+++ src/openrct2-unity/Assets/Scripts/UI/PeepInformation.cs
@@ -6,7 +6,12 @@ using UnityEngine.UI;
 public class PeepInformation : MonoBehaviour
 {
     private string name;
-    private string hunger;
+    private int happiness;
+    private int energy;
+    private int hunger;
+    private int thirst;
+    private int nausea;
+    private int toilet;
     [SerializeField] private GameObject peepBox;
     [SerializeField] private GameObject referenceObject;
     private GameObject PeepBox;
@@ -20,8 +25,21 @@ public class PeepInformation : MonoBehaviour
             PeepBox.name = $"PeepBox: {name}";
             var titleText = PeepBox.transform.Find("Header").transform.Find("TitleText");
             titleText.GetComponent<Text>().text = name;
-            var hungerText = PeepBox.transform.Find("Bottom").transform.Find("Page Area").transform.Find("Page 2 - Needs").transform.Find("Hunger");
-            hungerText.GetComponent<Text>().text = $"Hunger: {hunger}";
+            var needValues = PeepBox.transform.Find("Bottom").transform.Find("Page Area").transform.Find("Page 2 - Needs").transform.Find("NeedValues");
+            var happinessBar = needValues.transform.Find("HappinessBar");
+            var energyBar = needValues.transform.Find("EnergyBar");
+            var hungerBar = needValues.transform.Find("HungerBar");
+            var thirstBar = needValues.transform.Find("ThirstBar");
+            var nauseaBar = needValues.transform.Find("NauseaBar");
+            var toiletBar = needValues.transform.Find("ToiletBar");
+
+            happinessBar.GetComponent<Slider>().value = happiness;
+            energyBar.GetComponent<Slider>().value = energy;
+            hungerBar.GetComponent<Slider>().value = hunger;
+            thirstBar.GetComponent<Slider>().value = thirst;
+            nauseaBar.GetComponent<Slider>().value = nausea;
+            toiletBar.GetComponent<Slider>().value = toilet;
+
         }
 
     }
@@ -29,7 +47,13 @@ public class PeepInformation : MonoBehaviour
     public void UpdateInformation(OpenRCT2.Unity.Peep peep)
     {
         name = $"{peep.type} {peep.Id}";
-        hunger = $"{peep.hunger}";
+
+        happiness = peep.happiness;
+        energy = peep.energy;
+        hunger = peep.hunger;
+        thirst = peep.thirst;
+        nausea = peep.nausea;
+        toilet = peep.toilet;
         /*
         var intensityBinary = System.Convert.ToString(peep.intensity, 2);
         var maxIntensityString = System.Convert.ToString(peep.intensity >> 4);
